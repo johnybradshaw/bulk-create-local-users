528d95b6b659d49e84002b38e26a2ad604fd0fee
John <johnybradshaw@users.noreply.github.com>
2022-09-08 19:59:30 +0100

    Fix for full name

--- bulk.ps1
+++ bulk.ps1
@@ -1,15 +1,15 @@
 Add-Type -Assembly System.Web
 #Import a CSV file of users
-$CSV = Import-Csv "~\users.csv"
+$CSV = Import-Csv ".\users.csv"
 Write-Host “Username : Password” -foregroundcolor red -backgroundcolor yellow
 foreach($LINE in $CSV)
 {
     $NewUser="$($LINE.USERNAME)"
     $UsersName="$($LINE.NAME)"
     # $NewPass="$($LINE.PASSWORD)" 
-    # $SecurePass=ConvertTo-SecureString –AsPlainText -Force -String "$NewPass"
     $pass=[Web.Security.Membership]::GeneratePassword(32,0)
-    New-LocalUser -Name $NewUser -Password $pass -Name $UsersName
+    $SecurePass=ConvertTo-SecureString –AsPlainText -Force -String "$pass"
+    New-LocalUser -Name $NewUser -Password $SecurePass -FullName $UsersName
     #Add created user to the local admin group
     Add-LocalGroupMember -Group "Administrators" -Member $NewUser
     #Print out the username and password
